32722e926d272e73623e7c2fffa3585dfe66ff8d
vladislemon <vladislemon@gmail.com>
2020-11-14 16:56:27 +0300

    Initial commit

---
+++ src/main/java/net/lemoncookie/heavyindustry/HeavyIndustry.java
@@ -0,0 +1,10 @@
+package net.lemoncookie.heavyindustry;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HeavyIndustry implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/lemoncookie/heavyindustry/client/HeavyIndustryClient.java
@@ -0,0 +1,13 @@
+package net.lemoncookie.heavyindustry.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class HeavyIndustryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
